e448f83d5d755cf5bdf694238ab372358ded2b73
szaboerik <78268337+szaboerik@users.noreply.github.com>
2021-11-02 12:48:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,52 +3,42 @@ var termekTomb = [];
 $(function () {
 
 
-    $("aside").on("click", ".torol", function () {
+    $("article").on("click", "torol", function () {
         var index = Number($(this).attr("index"));
         termekTomb.splice(index,1);
-        termekMegjelenit();
-
 
 
 
             
 });
-$.ajax(
-    {url: "termekek.json", success: function (result) {
-            console.log(result);
-            termekTomb = result;
-            termekMegjelenit();
-
+$(document).ready(function (){
+    $.getJSON("termekek.json" , function(data){
+        var termekekData = '';
+        $.each(data , function(key,value){
+            termekekData += '<tr>';
+            termekekData += '<td>'+value.nev+'</td>';
+            termekekData += '<td>'+value.leiras+'</td>';
+            termekekData += '<td>'+value.ar+'</td>';
+            termekekData +='<td>'+value.keszlet+'</td>';
+            termekekData +='<td><input type=button id=modosit value=Módosítás></td>';
+            termekekData +='<td><input type=button id=torol value=Törlés></td>';
+        });
+        $('#termekekTablazat').append(termekekData);
+    });
+    $.ajax(
+        {url: "termekek.json", success: function (result) {
+                console.log(result);
+                termekTomb = result;
          
-        }});
+            }});
+
 });
-function ujTermek(){
-  
-    var termekObjektum = {
-    nev: $("#termeknev").val(),
-    leiras: $("#leiras").val(),
-    ar: $("#ar").val(),
-    keszlet: $("#keszlet").val()                    
-    };       
-    termekTomb.push(termekObjektum);
-    termekMegjelenit();
-}
+
+
+});
 
 
 
-
-function termekMegjelenit() {
-    $("aside").empty();
-    $('#ok').click(ujTermek);
-    for (var item in termekTomb) {
-    var termekek =  "<table><tr><td>Terméknéve</td><td>Leírás</td>><td>Készlet</td><td>Ár</td></tr><tr><th>"+termekTomb[item]["nev"] +
-    "</th><th>"+termekTomb[item]["leiras"] + "</th><th>"+termekTomb[item]["keszlet"] + "</th><th>"+termekTomb[item]["ar"]+"</th><th><input type='button' value='Módosít' class='modosit'></th><th><input type='button' value='Törlés' class='torol'</th></tr></table>"
-        $("aside").append("<table='"+termekTomb[item][""] +"'>"+termekek+"</table>");
-        
-    
-    }
-    
-}
 
 
 
