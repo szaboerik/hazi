77d547c5348271a104767fdab7920c0a4780d3c2
szaboerik <78268337+szaboerik@users.noreply.github.com>
2021-11-02 11:44:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function termekMegjelenit() {
     $("aside").empty();
     $('#ok').click(ujTermek);
     for (var item in termekTomb) {
-    var termekek =  "<table><tr><td>Terméknév</td><td>Leírás</td>><td>Készlet</td><td>Ár</td></tr><tr><th>"+termekTomb[item]["nev"] +
+    var termekek =  "<table><tr><td>Terméknéve</td><td>Leírás</td>><td>Készlet</td><td>Ár</td></tr><tr><th>"+termekTomb[item]["nev"] +
     "</th><th>"+termekTomb[item]["leiras"] + "</th><th>"+termekTomb[item]["keszlet"] + "</th><th>"+termekTomb[item]["ar"]+"</th><th><input type='button' value='Módosít' class='modosit'></th><th><input type='button' value='Törlés' class='torol'</th></tr></table>"
         $("aside").append("<table='"+termekTomb[item][""] +"'>"+termekek+"</table>");
         
